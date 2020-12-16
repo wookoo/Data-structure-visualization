917d8640534b3e306d6e0dfd815efdf644fa3b22
wookoo <nanayagoon@daum.net>
2020-12-17 07:24:24 +0900

    보드 초기화 구현

--- StackLinkedList.js
+++ StackLinkedList.js
@@ -1,3 +1,6 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
 class Node{
   constructor(value,next){
     this.value = value;
@@ -9,21 +12,42 @@ class StackLinkedList{
 
   constructor(){
 
-
-
-    this.navigator = [addcontrolEntity("Button","push"),
-    addcontrolEntity("Button","pop"),
-    addcontrolEntity("Button","clear")
+    this.pushField = addcontrolEntity("Text","");
+    this.pushField.size = 3;
+    this.pushField.oninput = this.pushFieldLimit.bind(this);
+    this.pushButton = addcontrolEntity("Button","push");
+    this.pushButton.onclick = this.push.bind(this);
+    this.popButton = addcontrolEntity("Button","pop");
+    this.popButton.onclick = this.pop.bind(this);
+    this.resetButton = addcontrolEntity("Button","reset");
+    this.resetButton.onclick = this.reset.bind(this);
+    this.navigator = [
+      this.pushField,
+      this.pushButton,
+      this.popButton,
+      this.resetButton
   ];
     this.top = null;
     this.stack = []; //
+    this.initBoard();
   }
-  push(value){ //푸쉬 버튼을 눌렀을떄 할일
-    if(this.stack.length == 7){//메모리가 가득 찼다고 가정
+  pushFieldLimit(){ //입력갯수 제한
+    if(this.pushField.value.length > 3){
+      this.pushField.value = this.pushField.value.slice(0,3);
+    }
+  }
+
+  push(){ //푸쉬 버튼을 눌렀을떄 할일
+
+    if(this.stack.length == 7){//메모리가 가득 차거나 입력이 없는 경우
       // TODO: 메모리가 가득차서 추가불가능
       return;
     }
-
+    var value = this.pushField.value.trim();
+    if(!Boolean(value)){//입력값이 없는 경우
+      return; //그냥 종료
+    }
+    console.log(value);
     var newNode = new Node(value,this.top); //새로운 노드 생성
     this.top = newNode; //top 변환, push 끝
     this.stack.push(newNode); //현재 까지 생성된 정보 스택에 넣기
@@ -31,6 +55,8 @@ class StackLinkedList{
     // TODO: 변화하는 과정 그리기 그리는동안 푸쉬 팝 불가
     this.disableUI();
     // TODO:  그리기 시작
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.drawTop();
 
     this.enableUI();
 
@@ -47,6 +73,13 @@ class StackLinkedList{
     // TODO:  그리기 시작
 
     this.enableUI();
+  }
+  reset(){//리셋 버튼을 눌렀을때 할 일
+
+    this.top = null;
+    this.stack = [];
+    this.initBoard();
+    // TODO: 보드 초기화
 
   }
 
@@ -63,6 +96,43 @@ class StackLinkedList{
   }
 
 
+  initBoard(){
+    // TODO: 보드 초기화 그리기
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.drawTop();
+  }
+  drawTop(){
+    ctx.beginPath();
+    ctx.rect(50,50,50,50)
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.font = '10px Arial';
+    ctx.fillText("TOP",60,120);
+
+    if(!this.top){
+      ctx.moveTo(50,50);
+      ctx.lineTo(100,100);
+    }
+    ctx.stroke();
+
+    ctx.closePath();
+  }
+
+  drawNode(x,y){ //노드 그리기
+    ctx.beginPath();
+    ctx.rect(x,y,100,50)
+
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.moveTo(x+70,y)
+    ctx.lineTo(x+70,y+50)
+    ctx.stroke();
+    ctx.closePath();
+  }
+  drawPush(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
 
 
 }
+
+var k = new StackLinkedList();
