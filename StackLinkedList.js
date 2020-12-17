e1c388135420cfc9aa856cf05f80b97e7a9b6426
wookoo <nanayagoon@daum.net>
2020-12-17 20:24:56 +0900

    연결리스트 기반 스택 삽입

--- StackLinkedList.js
+++ StackLinkedList.js
@@ -27,10 +27,33 @@ class StackLinkedList{
       this.popButton,
       this.resetButton
   ];
+  this.backButton = document.getElementById("back");
+  this.backButton.disabled = true;
+  this.nextButton = document.getElementById("next");
+  this.nextButton.onclick = this.nextButtonClick.bind(this);
+  this.nextButton.disabled = true;
+
+
+
     this.top = null;
     this.stack = []; //
+
+    this.drawTask=[];
+
     this.initBoard();
   }
+  nextButtonClick(){
+    if(this.drawTask.length == 0){
+      return;
+    }
+    this.drawTask[0]();
+    this.drawTask.splice(0,1);
+    if(this.drawTask.length == 0){
+      this.enableUI();
+      this.nextButton.disabled = true;
+    }
+  }
+
   pushFieldLimit(){ //입력갯수 제한
     if(this.pushField.value.length > 3){
       this.pushField.value = this.pushField.value.slice(0,3);
@@ -39,8 +62,13 @@ class StackLinkedList{
 
   push(){ //푸쉬 버튼을 눌렀을떄 할일
 
-    if(this.stack.length == 7){//메모리가 가득 차거나 입력이 없는 경우
+    if(this.stack.length == 8){//메모리가 가득 차거나 입력이 없는 경우
       // TODO: 메모리가 가득차서 추가불가능
+
+      this.initBoard();
+      this.drawStack();
+      ctx.font = '10px Arial';
+      ctx.fillText("스택이 가득차서 삽입 불가! (할당 가능공간 없음)" ,20,30);
       return;
     }
     var value = this.pushField.value.trim();
@@ -48,37 +76,78 @@ class StackLinkedList{
       return; //그냥 종료
     }
     console.log(value);
+
+    this.disableUI();
     var newNode = new Node(value,this.top); //새로운 노드 생성
     this.top = newNode; //top 변환, push 끝
     this.stack.push(newNode); //현재 까지 생성된 정보 스택에 넣기
 
     // TODO: 변화하는 과정 그리기 그리는동안 푸쉬 팝 불가
+    this.nextButton.disabled = false;
     this.disableUI();
+    var x = 900-(this.stack.length)*100;
+    this.drawTask.push(()=>{this.drawNode(x,150,this.top.value)});//그리기 작업 추가
+    this.drawTask.push(()=>{this.drawline(x+60,180,x+110,180)}); //그리기 작업 추가
+    this.drawTask.push(()=>{this.drawStack();
+      ctx.fillText("삽입된 값 : " + this.top.value  ,20,30);
+    }); //그리기 작업 추가
     // TODO:  그리기 시작
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.drawTop();
+    //this.drawStack();
 
-    this.enableUI();
+
 
   }
   pop(){//pop 버튼을 눌렀을때 할일
     if(!this.top){//스택이 비었으면
       // TODO: 스택이 비어서 삭제 불가
+      this.initBoard();
+      ctx.font = '10px Arial';
+      ctx.fillText("스택이 비어서 삭제 불가!" ,20,30);
       return;
     }
+    this.returnData = this.top.value;
     this.top = this.top.next;
-    this.stack.splice(0,1);
+    this.stack.splice(this.stack.length-1,1);
     // TODO: 변화하는 과정 그리기, 그리는동안 푸쉬 팝 불가
+    this.nextButton.disabled = false;
     this.disableUI();
-    // TODO:  그리기 시작
 
-    this.enableUI();
+    // TODO:  그리기 시작
+    this.drawTask.push(()=>{
+      ctx.font = '10px Arial';
+      ctx.fillStyle = "#fef";
+      ctx.fillRect(20,5,100,30);
+      ctx.fillStyle = "#000";
+      ctx.fillText("반환값 : " + this.returnData,20,30);
+    })
+    this.drawTask.push(()=>{
+      ctx.beginPath();
+      ctx.font = '10px Arial';
+      ctx.fillText("TOP",900-(this.stack.length)*100+20,220);
+      ctx.fillStyle = "#fef"; //#fef
+      ctx.fillRect(900-(this.stack.length+1)*100,210,50,50);
+      ctx.fill();
+      ctx.closePath();
+    });
+    this.drawTask.push(()=>{
+      ctx.fillStyle = "#000";
+      this.drawStack();
+      ctx.fillText("반환값 : " + this.returnData,20,30);
+    }
+
+    );
+
+
+
+    //this.enableUI();
   }
   reset(){//리셋 버튼을 눌렀을때 할 일
 
     this.top = null;
     this.stack = [];
     this.initBoard();
+    this.nextButton.disabled = true;
+    this.backButton.disabled = true;
     // TODO: 보드 초기화
 
   }
@@ -99,36 +168,59 @@ class StackLinkedList{
   initBoard(){
     // TODO: 보드 초기화 그리기
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.drawTop();
-  }
-  drawTop(){
-    ctx.beginPath();
-    ctx.rect(50,50,50,50)
-    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
     ctx.font = '10px Arial';
-    ctx.fillText("TOP",60,120);
-
-    if(!this.top){
-      ctx.moveTo(50,50);
-      ctx.lineTo(100,100);
-    }
-    ctx.stroke();
-
-    ctx.closePath();
+    ctx.fillText("TOP",910,220);
+    this.drawNullBox()
   }
 
-  drawNode(x,y){ //노드 그리기
+
+  drawNode(x,y,value){ //노드 그리기
     ctx.beginPath();
-    ctx.rect(x,y,100,50)
+    ctx.rect(x,y,70,50)
 
     ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.moveTo(x+70,y)
-    ctx.lineTo(x+70,y+50)
+    ctx.moveTo(x+50,y)
+    ctx.lineTo(x+50,y+50)
+    ctx.stroke();
+    ctx.font = '10px Arial';
+    ctx.fillText(value,x+20,y+30);
+    ctx.closePath();
+  }
+  drawStack(){
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(var i = 0; i <this.stack.length;i++){
+      var x = 900-(this.stack.length-i)*100;
+      this.drawNode(x,150,this.stack[this.stack.length-i-1].value);
+      this.drawline(x+60,180,x+110,180);
+    }
+    ctx.beginPath();
+    ctx.font = '10px Arial';
+    ctx.fillText("TOP",900-(this.stack.length)*100+20,220);
+    ctx.fillStyle="#000";
+    ctx.closePath();
+    this.drawNullBox();
+  }
+  drawline(x1,y1,x2,y2){
+    ctx.beginPath();
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
     ctx.stroke();
     ctx.closePath();
   }
-  drawPush(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawNullBox(){
+    ctx.beginPath();
+    ctx.rect(900,150,50,50)
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+
+
+    ctx.moveTo(900,150);
+    ctx.lineTo(950,200);
+
+    ctx.stroke();
+    ctx.closePath();
+
   }
 
 
